de902cb344ce3ef75e275db2156c766368f80b51
Davi Munhoz <davimunhoz1005@gmail.com>
2025-11-24 23:32:56 -0300

    feat(vehicle-entity): creates vehicle entity with vehicle status and default infos

---
+++ src/main/java/com/drivefleet/drivefleet/domain/entities/Vehicle.java
@@ -0,0 +1,51 @@
+package com.drivefleet.drivefleet.domain.entities;
+
+import com.drivefleet.drivefleet.domain.enums.VehicleStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_SELLER")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Vehicle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(columnDefinition = "CHAR(36)")
+    private UUID id;
+
+    @Column(nullable = false)
+    private String brand;
+
+    @Column(nullable = false)
+    private String model;
+
+    @Column(nullable = false)
+    private Integer yearManufacture;
+
+    @Column(nullable = false)
+    private Integer yearModel;
+
+    @Column(nullable = false, unique = true)
+    private String plate;
+
+    @Column(nullable = false)
+    private String color;
+
+    @Column(nullable = false)
+    private Double mileage;
+
+    @Column(nullable = false, precision = 8, scale = 2)
+    private BigDecimal value;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private VehicleStatus status;
+}
