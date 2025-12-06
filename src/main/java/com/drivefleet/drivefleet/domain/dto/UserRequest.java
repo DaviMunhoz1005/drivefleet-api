41e842d37d4c6f132b33298ee518e2dd20586fd8
Davi Munhoz <davimunhoz1005@gmail.com>
2025-12-06 12:01:21 -0300

    feat(user-dto-request): create user dto to make a request

---
+++ src/main/java/com/drivefleet/drivefleet/domain/dto/UserRequest.java
@@ -0,0 +1,25 @@
+package com.drivefleet.drivefleet.domain.dto;
+
+import com.drivefleet.drivefleet.domain.enums.UserRole;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record UserRequest(
+        @NotBlank(message = "Name is required")
+        String name,
+
+        @NotBlank(message = "Email is required")
+        @Email(message = "Email must be valid")
+        String email,
+
+        @NotBlank(message = "Password is required")
+        @Size(min = 8, message = "Password must have at least 8 characters")
+        String password,
+
+        @NotNull(message = "Role is required")
+        UserRole role
+) {}
+
+
