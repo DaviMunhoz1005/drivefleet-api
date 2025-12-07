a38cb55b005fde9a2c14456844404c25eaf8e426
Davi Munhoz <davimunhoz1005@gmail.com>
2025-12-07 17:33:19 -0300

    chore(seller-service-delete): implements delete method to make a logical exclusion

--- src/main/java/com/drivefleet/drivefleet/service/SellerService.java
+++ src/main/java/com/drivefleet/drivefleet/service/SellerService.java
@@ -4,11 +4,16 @@ import com.drivefleet.drivefleet.domain.dto.seller.SellerRequest;
 import com.drivefleet.drivefleet.domain.dto.seller.SellerResponse;
 import com.drivefleet.drivefleet.domain.entities.Seller;
 import com.drivefleet.drivefleet.domain.entities.User;
+import com.drivefleet.drivefleet.domain.enums.UserStatus;
+import com.drivefleet.drivefleet.exceptions.SellerCannotBeExcludedException;
+import com.drivefleet.drivefleet.exceptions.UserNotFoundIdException;
 import com.drivefleet.drivefleet.repository.SellerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Service
@@ -18,6 +23,27 @@ public class SellerService {
     private final SellerRepository sellerRepository;
     private final UserService userService;
     private final SalesOrderService salesOrderService;
+
+    public SellerResponse create(SellerRequest request) {
+        User user = userService.create(request.user());
+        Seller seller = Seller.builder()
+                .registrationNumber(generateRegistrationNumber())
+                .user(user)
+                .sales(new ArrayList<>())
+                .build();
+        sellerRepository.save(seller);
+        return convertToResponse(seller);
+    }
+
+    @Transactional
+    public void deleteById(UUID id) {
+        Seller seller = sellerRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundIdException(id.toString()));
+        if (!seller.getSales().isEmpty()) {
+            throw new SellerCannotBeExcludedException(id.toString());
+        }
+        seller.getUser().setStatus(UserStatus.EXCLUDED);
+    }
 
     private SellerResponse convertToResponse(Seller seller) {
         return SellerResponse.builder()
@@ -39,16 +65,5 @@ public class SellerService {
                     .nextLong(10_000_000L, 99_999_999L);
         } while (sellerRepository.existsByRegistrationNumber(number));
         return number;
-    }
-
-    public SellerResponse create(SellerRequest request) {
-        User user = userService.create(request.user());
-        Seller seller = Seller.builder()
-                .registrationNumber(generateRegistrationNumber())
-                .user(user)
-                .sales(new ArrayList<>())
-                .build();
-        sellerRepository.save(seller);
-        return convertToResponse(seller);
     }
 }
